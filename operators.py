80b630c6ad6449450ec679eee84afc1763875086
Sh2rif <sh2rif@gmail.com>
2020-06-22 14:18:15 +0300

    logical operators

---
+++ operators.py
@@ -0,0 +1,28 @@
+num1 = 10
+num2= 30
+
+# print(num1 > num2)
+# print(num1 < num2)
+# print(10 == 30)
+# print(10 != 10)
+# print(10 <= 30)
+# print(10 >= 30)
+a,b = 1,2
+print(a == a and b == b) #True
+print(a == a and a < b) #True
+
+# print(True and True) #True
+# print(True and False) #False
+# print(False and False) #False
+
+# print(True or True) #True
+# print(True or False) #True
+# print(False or False) #False
+print(a == a or b == b) #True
+print(a == a or a < b) #True
+
+
+# print(not False) #True
+# print(not True) #False
+print(not (a != 5)) #False
+print(not (a == 5)) #True
