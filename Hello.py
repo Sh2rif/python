05b9c5757ad02fe3e4987b36bb5341aac83a23ff
Sh2rif <sh2rif@gmail.com>
2020-06-22 12:11:32 +0300

    hello world and multiline new line

--- Hello.py
+++ Hello.py
@@ -1,1 +1,6 @@
-print('Hello')
+#this is hello world application
+print('Hello world! \n')
+print("Hello world! 2 \n")
+# Multiline
+print('''Hello im sharif this day
+is hot weather and this multiline''')
