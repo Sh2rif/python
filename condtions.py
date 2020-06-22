e1ccf16434e02a2ab9d2370058fff9515995144e
Sh2rif <sh2rif@gmail.com>
2020-06-22 14:47:48 +0300

    Condtions.py

---
+++ condtions.py
@@ -0,0 +1,6 @@
+username = input ('Please insert your username')
+password = input ('Please insert your password')
+if username == 'sharif' and password == '123':
+    print('welcome')
+else:
+    print('Error')
